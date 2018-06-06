dd692e699a806d96cc021f360e009fd968b52f99
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-06 11:15:40 -0500

    Added append functions at end

---
+++ financial-interests.js
@@ -0,0 +1,14 @@
+// Create an h2 element with the title of the section in it
+const h2 = document.createElement("h2");
+h2.textContent = "Financial Interests";
+
+// Create an array with the h2 element as it's first item
+const ehArray = [h2];
+
+
+// Make variables for a section
+
+
+// Pass those variables to createSection: const sectionName = createSection(var1, var2)
+// Push that section to the array: array.push(sectionName)
+// Rinse and repeat

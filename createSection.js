392b3659b817a195b47ac3d16d98f3ae2172d2f7
Robert Leedy <leedyrj@gmail.com>
2018-06-06 10:41:16 -0500

    added createSection.js

---
+++ createSection.js
@@ -0,0 +1,18 @@
+//function takes two paramaters and makes text of first parameter an h3
+//second parameter is an array that is turned into an unordered list
+
+const createSection = (param1, param2) => {
+    const section = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const ul = document.createElement('ul');
+    h3.textContent = param1;
+    section.appendChild(h3)
+    for (let i=0; i<param2.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = param2[i];
+        ul.appendChild(li);
+    }
+    section.appendChild(ul);
+    return section
+}
+

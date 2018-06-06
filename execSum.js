d6f202c0ac5b68b17aecdf578a671e8a40af01bd
Robert Leedy <leedyrj@gmail.com>
2018-06-06 11:16:19 -0500

    executive summary commit

---
+++ execSum.js
@@ -0,0 +1,12 @@
+// Create an h2 element with the title of the section in it
+
+
+// Create an array with the h2 element as it's first item
+
+
+// Make variables for a section
+// Pass those variables to createSection: const sectionName = createSection(var1, var2)
+// Push that section to the array: array.push(sectionName)
+// Rinse and repeat
+
+const rlArray = []

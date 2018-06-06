633183642c886f0de0128a169acbaf36c2057573
Robert Leedy <leedyrj@gmail.com>
2018-06-06 11:45:45 -0500

    finished known aliases section

--- execSum.js
+++ execSum.js
@@ -1,12 +1,35 @@
 // Create an h2 element with the title of the section in it
-
+const rlExecH2 = document.createElement('h2');
+rlExecH2.textContent = "Executive Summary"
 
 // Create an array with the h2 element as it's first item
-
+const rlArray = []
 
 // Make variables for a section
+const rlAliasHead = 'Known Aliases'
+const rlAliasList = ['R. Alessandro Torington', 'King Rodge Toyne', 'Ruben A. Tozzi', 'Torosian Rubblood']
+const rlAliasFunction = createSection(rlAliasHead, rlAliasList)
+const rlAliasSection = [rlAliasFunction]
 // Pass those variables to createSection: const sectionName = createSection(var1, var2)
 // Push that section to the array: array.push(sectionName)
 // Rinse and repeat
 
-const rlArray = []
+rlArray.push(rlExecH2, rlAliasFunction)
+
+
+
+// const createSection = (param1, param2) => {
+//     const section = document.createElement('section');
+//     const h3 = document.createElement('h3');
+//     const ul = document.createElement('ul');
+//     h3.textContent = param1;
+//     section.appendChild(h3)
+//     for (let i=0; i<param2.length; i++) {
+//         let li = document.createElement('li');
+//         li.textContent = param2[i];
+//         ul.appendChild(li);
+//     }
+//     section.appendChild(ul);
+//     return section
+// }
+

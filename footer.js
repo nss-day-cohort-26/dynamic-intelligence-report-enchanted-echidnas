0fd35a58a1623c55f15bebdacec55ffce94d14b2
Robert Leedy <leedyrj@gmail.com>
2018-06-07 14:40:36 -0500

    added footer with mad styles

---
+++ footer.js
@@ -0,0 +1,5 @@
+const footerCopy = '\u00A92018'
+const footerNames = ['Elliot Huck |', 'Robert Leedy |', 'Jacob Henderson |', 'Brett Shearin']
+const footer = createSection(footerCopy, footerNames)
+footer.setAttribute('class', 'footer')
+const footerArray = [footer]

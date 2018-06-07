74c019fb68e05eb77a43b7c0938b40805ec620c4
Brett Shearin <brettcshearin@hotmail.com>
2018-06-07 10:56:17 -0500

    added comments and the rest of the css

--- append.js
+++ append.js
@@ -1,11 +1,13 @@
-let artSel = document.querySelector("#container")
-function append(array) {  
-    let frag = document.createDocumentFragment()
-    for (i=0; i < array.length; i++) {
-        frag.appendChild(array[i])
-    }
+let artSel = document.querySelector("#container") 
+function append(array) {  //...takes 1 argument: an array of DOM <section> elements
+    let frag = document.createDocumentFragment() //...creates a document fragment
+    for (i=0; i < array.length; i++) { //...loops through the array of <section> elements and appends each one to the fragment
+        frag.appendChild(array[i]) 
+    }                               //...appends the fragment to the DOM
     artSel.appendChild(frag)
 }
+//The page then calls the append( ) function on the master arrays that each individual page produces
+
 // append(testArray)
 // append(arrayTest)
 append(navArray)
